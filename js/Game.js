0d195952586a2a7d21e0cb0323c52045359a4c73
Voke Elstein <141534071+rainytides@users.noreply.github.com>
2023-10-03 14:08:11 -0700

    Almost there

--- js/Game.js
+++ js/Game.js
@@ -49,42 +49,78 @@ class Game {
         const hearts = document.querySelectorAll('.tries img');
         hearts[this.missed].src = 'images/lostHeart.png';
         this.missed += 1;
+    
+        // Check if only two hearts are left
+        if (this.missed === 3) {
+            const remainingHearts = document.querySelectorAll('.tries img[src="images/liveHeart.png"]');
+            remainingHearts.forEach(heart => heart.classList.add('flashing'));
+        }
+    
         if (this.missed === 5) {
             this.gameOver(false);
         }
     }
-
     // Add the checkForWin() method
     checkForWin() {
         const hiddenLetters = document.querySelectorAll('.hide');
         return hiddenLetters.length === 0;
     }
 
+    // Add the revealPhrase() method
+    revealPhrase() {
+        const phraseDiv = document.querySelector('#phrase ul');
+        phraseDiv.innerHTML = '';
+        this.activePhrase.addPhraseToDisplay();
+        const letters = document.querySelectorAll('#phrase li');
+        letters.forEach(letter => {
+            if (letter.className.includes('hide')) {
+                letter.classList.remove('hide');
+                letter.classList.add('show');
+            }
+        });
+    }
+
     // Add the gameOver() method
     gameOver(gameWon) {
         const overlay = document.querySelector('#overlay');
         const gameOverMessage = document.querySelector('#game-over-message');
-        overlay.style.display = 'block';
+        
+        // Hide the overlay immediately
+        overlay.style.display = 'none';
+    
+        // Reveal the phrase only if the user won
         if (gameWon) {
-            gameOverMessage.textContent = 'Congratulations! You won!';
-            overlay.className = 'win';
+            this.revealPhrase();
+            setTimeout(() => {
+                overlay.style.display = 'block';
+                gameOverMessage.textContent = 'Congratulations! You won!';
+                overlay.className = 'win';
+                this.resetGame();
+            }, 4000);
         } else {
+            // Display the lost message immediately
+            overlay.style.display = 'block';
             gameOverMessage.textContent = 'Sorry, better luck next time!';
             overlay.className = 'lose';
+            this.resetGame();
         }
-        this.resetGame();
     }
-
+    
     // Add the resetGame() method
     resetGame() {
-        const phraseDiv = document.querySelector('#phrase ul');
-        const hearts = document.querySelectorAll('.tries img');
-        const keys = document.querySelectorAll('.key');
-        phraseDiv.innerHTML = '';
-        hearts.forEach(heart => heart.src = 'images/liveHeart.png');
-        keys.forEach(key => {
-            key.className = 'key';
-            key.disabled = false;
-        });
-    }
+    const phraseDiv = document.querySelector('#phrase ul');
+    const hearts = document.querySelectorAll('.tries img');
+    const keys = document.querySelectorAll('.key');
+    
+    phraseDiv.innerHTML = '';
+    hearts.forEach(heart => {
+        heart.src = 'images/liveHeart.png';
+        heart.classList.remove('flashing');  // Stop hearts from flashing
+    });
+    
+    keys.forEach(key => {
+        key.className = 'key';
+        key.disabled = false;
+    });
+}
 }
